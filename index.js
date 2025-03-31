b0095f18a072bf1898c9017cf1c13c219aa124f1
James <118614403+JumesP@users.noreply.github.com>
2025-03-31 14:08:07 +0100

    basic express index.js init

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require('express');
+const router = express.Router();
+const env = process.env;
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+router.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(env.PORT, () => {
+    console.log(`Server is running on port ${env.PORT}`);
+})
