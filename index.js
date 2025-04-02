35e0413ffce423786ad8ef7b344c093d01849c52
James <118614403+JumesP@users.noreply.github.com>
2025-04-02 18:03:29 +0100

    minor index.js changes including:
    mongoose implemention
    env renaming

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 require('dotenv').config();
-
 const express = require('express');
-const router = express.Router();
+const cors = require('cors');
+const mongoose = require('mongoose');
 const env = process.env;
 
 const app = express();
+const PORT = env.PORT || 4000;
 
-app.use(express.static(__dirname + '/public'));
+// middleware
+app.use(cors());
+app.use(express.json());
 
-router.get('/', (req, res) => {
+// database connection (using mongoose)
+
+mongoose.connect(env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.log(err));
+
+app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(env.PORT, () => {
-    console.log(`Server is running on port ${env.PORT}`);
 // Routes
 const authRouter = require("./src/routes/authRoutes");
 app.use("/auth", authRouter);
 
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
