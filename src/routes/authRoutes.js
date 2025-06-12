25dbcb73557e9fc3544eafa18922a3734ebbc43d
James <118614403+JumesP@users.noreply.github.com>
2025-06-12 11:40:05 +0100

    protected real functionality

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -9,6 +9,7 @@ router.use(bodyParser.urlencoded({ extended: true }));
 const { registerUser, loginUser } = require('../controllers/authController');
 
 const authenticationToken = require('../middlewares/authenticationToken');
+const { findUserById } = require('../models/userModel');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
@@ -17,10 +18,22 @@ router.get('/', (req, res) => {
     res.send('Base Route');
 });
 
-router.get('/protected', authenticationToken, (req, res) => {
-    // the response for this will include the UserID which can be used on the clients database of stored data
-    const userData = req.user;
-    res.json({ message: 'This is a protected route', user: userData });
+router.get('/protected', authenticationToken, async (req, res) => {
+    // Query the database for the user based on the user ID from the token
+    // Query the database for the user based on the user ID from the token using the User schema
+    console.log(req.user.id);
+    const userId = req.user && (req.user.id || req.user._id);
+
+    console.log("User ID from token:", userId);
+
+    try {
+        const user = await findUserById('TrainEvoo', userId);
+        console.log("User model:", user);
+        console.log("User id:", user.userID);
+        res.json({message: 'This is a protected route', userID: user ? user.userID : null});
+    } catch (error) {
+        res.status(500).json({error: 'Failed to fetch user data'});
+    }
 });
 
 module.exports = router;
