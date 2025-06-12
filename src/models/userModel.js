a2257032a3e005b04a21f4630198e71ce2cd6c22
James <118614403+JumesP@users.noreply.github.com>
2025-06-12 11:39:21 +0100

    added extra stuffs to user model, CRUD related stuff

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -11,11 +11,59 @@ const createUserModel = (applicationName) => {
     const userSchema = new Schema({
         username: { type: String, required: true, unique: true },
         password: { type: String, required: true },
-        userID: { type: Number, required: true },
+        userID: { type: String, required: true },
     }, { timestamps: true });
 
     return mongoose.model(modelName, userSchema);
 };
 
-module.exports = createUserModel;
+const queryUserModel = async (applicationName, query) => {
+    const User = createUserModel(applicationName);
+    if (!User) {
+        throw new Error(`Model for application '${applicationName}' could not be created.`);
+    }
+    return await User.find(query).exec();
+};
 
+const findUserById = async (applicationName, userId) => {
+    const User = createUserModel(applicationName);
+    if (!User) {
+        throw new Error(`Model for application '${applicationName}' could not be created.`);
+    }
+    return await User.findById(userId).exec();
+};
+
+const findUserByUsername = async (applicationName, username) => {
+    const User = createUserModel(applicationName);
+    if (!User) {
+        throw new Error(`Model for application '${applicationName}' could not be created.`);
+    }
+    return await User.findOne({ username }).exec();
+};
+
+const createNewUser = async (applicationName, userData) => {
+    const User = createUserModel(applicationName);
+    if (!User) {
+        throw new Error(`Model for application '${applicationName}' could not be created.`);
+    }
+    const newUser = new User(userData);
+    return await newUser.save();
+};
+
+const updateUser = async (applicationName, query, updateData) => {
+    const User = createUserModel(applicationName);
+    if (!User) {
+        throw new Error(`Model for application '${applicationName}' could not be created.`);
+    }
+    return await User.updateOne(query, updateData).exec();
+};
+
+const deleteUser = async (applicationName, query) => {
+    const User = createUserModel(applicationName);
+    if (!User) {
+        throw new Error(`Model for application '${applicationName}' could not be created.`);
+    }
+    return await User.deleteOne(query).exec();
+};
+
+module.exports = { createUserModel, queryUserModel, findUserById, findUserByUsername, createNewUser, updateUser, deleteUser };
